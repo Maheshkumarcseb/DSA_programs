7e7cdb5fbdc0014f1df2a303363b5128d25cfe71
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2024-03-12 18:17:13 +0530

    first commit

---
+++ reverse_iterative_LL.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* Reverse(struct Node* head) {
+    struct Node* current = head;
+    struct Node* prev = NULL;
+    struct Node* next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    return head;
+}
+
+struct Node* Insert(struct Node* head, int data) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->next = NULL;
+    if (head == NULL)
+        head = temp;
+    else {
+        struct Node* temp1 = head;
+        while (temp1->next != NULL)
+            temp1 = temp1->next;
+        temp1->next = temp;
+    }
+    return head;
+}
+
+void print(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    head = Insert(head, 2);
+    head = Insert(head, 4);
+    head = Insert(head, 5);
+    head = Insert(head, 8);
+    printf("Linked list before reverse: ");
+    print(head);
+    head = Reverse(head);
+    printf("Linked list after reverse: ");
+    print(head);
+
+    // Freeing memory
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
+}
